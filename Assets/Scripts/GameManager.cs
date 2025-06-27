f3172244f63a6ff29a8880e8e45be04b5d9542e0
DiazAle5 <youarereta@gmail.com>
2025-06-27 14:14:45 -0400

    Fixed GameManager conflict & Added Spline Package

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,4 +5,21 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public int currentLevel = 1;
+
+    void OnEnable()
+    {
+        TypingInput.OnWordCompleted += HandleWordCompletion;
+    }
+
+    void OnDisable()
+    {
+        TypingInput.OnWordCompleted -= HandleWordCompletion;
+    }
+
+    // TODO: Handle event on completed word - animation? visual indication? And launch an Update of UI of health bar and progress
+    // -- Health bar and progress should only be updated on specific events being recieved, not on an update call as any change will be a direct result of a user input
+    void HandleWordCompletion(string completedWord)
+    {
+        Debug.Log("Completed word: " + completedWord);
+    }
 }
