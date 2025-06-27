383b11d1fe9121982a2f55f66c5029d14c72e52c
rohanbhuyan <bhuyan.rohan@outlook.com>
2025-06-27 12:29:19 -0400

    handle LevelTwo scenario in typingConfig

--- Assets/TypingConfig.cs
+++ Assets/TypingConfig.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO: Consider extending TypingConfig to be an enum of multiple attributes, the actual word being just one element
 public static class TypingConfig
 {
     public static readonly List<string> LevelOneConfig = new List<string> { "random sentence fragment one" };
@@ -13,6 +14,8 @@ public static class TypingConfig
         {
             case "LevelOne":
                 return LevelOneConfig;
+            case "LevelTwo":
+                return LevelTwoConfig;
             default:
                 return new List<string> {  "No config for given scenario" };
         }
