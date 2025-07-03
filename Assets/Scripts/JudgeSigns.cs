40292762a5272ccda9b812235aeadfa282401018
DiazAle5 <youarereta@gmail.com>
2025-07-03 16:29:49 -0400

    Made level layout and judges raising signs
    Saved both as Prefabs, both the individually pieces such as the boss, the table of judges, and the player, as well as a full prefab using them all called Level Layout in Assets > Prefabs

---
+++ Assets/Scripts/JudgeSigns.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JudgeSigns : MonoBehaviour
+{
+    public float levelLenght;
+    TypingScenario currentScenario;
+    int numberOfJudges = 5;
+    public float timerForEachJudge;
+    public float timer;
+    [SerializeField] GameObject[] xJudges;
+    [SerializeField] GameObject[] oJudges;
+    int currentJudgeToCycle = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        levelLenght = GameObject.Find("WordManager").GetComponent<WordManager>().currentScenario.timeLimit;
+    }
+
+    private void Start()
+    {
+        timerForEachJudge = levelLenght / numberOfJudges - .6f;
+        //Added the .6 to offset it from the last judge raising his sign as the level reaches 0 seconds
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timerForEachJudge)
+        {
+            timer = 0;
+            CycleNextJudge();
+        }
+    }
+
+    private void CycleNextJudge()
+    {
+        xJudges[currentJudgeToCycle].SetActive(false);
+        oJudges[currentJudgeToCycle].SetActive(true);
+        currentJudgeToCycle++;
+    }
+}
