62e74fe710c09c8475fa3ade805094ce7ae40393
rohanbhuyan <bhuyan.rohan@outlook.com>
2025-07-07 10:15:41 -0400

    intermediary changes before rebase

--- Assets/TypingConfig.cs
+++ Assets/TypingConfig.cs
@@ -11,31 +11,45 @@ public static class TypingConfig
         {
             case "LevelOne":
                 List<TypingLine>  LevelOneConfig = new List<TypingLine>();
-                LevelOneConfig.Add(new TypingLine(entity: "Enemy", textToType: "You are coming in late again? This is the fifth time this week. We could have you fired for this.", timeLimitSeconds: -1));
-                LevelOneConfig.Add(new TypingLine(entity: "Player", textToType: "Fired?", timeLimitSeconds: -1));
-                LevelOneConfig.Add(new TypingLine(entity: "Enemy", textToType: "Yes, fired. Gone, done, finished. Either shape up or leave!", timeLimitSeconds: -1));
-                LevelOneConfig.Add(new TypingLine(entity: "Player", textToType: "Heh, well, I guess I can reveal my plan then. I've been late because I’ve been gathering evidence. This corporation is corrupt all the way up to the top. And now, I’m going to challenge you to a type-off to claim my place at its head.", timeLimitSeconds: -1));
-                LevelOneConfig.Add(new TypingLine(entity: "Enemy", textToType: "Wh-What!? A type-off? Are you insane? This isn't a joke! A type-off is a serious thing! Just get back to your desk and get to work and I won't take this infraction any further!", timeLimitSeconds: -1));
-                LevelOneConfig.Add(new TypingLine(entity: "Player", textToType: "No", timeLimitSeconds: -1));
+                LevelOneConfig.Add(new TypingLine(entity: "Enemy", textToType: "You are coming in late again? This is the fifth time this week. We could have you fired for this."));
+                LevelOneConfig.Add(new TypingLine(entity: "Player", textToType: "Fired?"));
+                LevelOneConfig.Add(new TypingLine(entity: "Enemy", textToType: "Yes, fired. Gone, done, finished. Either shape up or leave!"));
+                LevelOneConfig.Add(new TypingLine(entity: "Player", textToType: "Heh, well, I guess I can reveal my plan then. I've been late because I’ve been gathering evidence. This corporation is corrupt all the way up to the top. And now, I’m going to challenge you to a type-off to claim my place at its head."));
+                LevelOneConfig.Add(new TypingLine(entity: "Enemy", textToType: "Wh-What!? A type-off? Are you insane? This isn't a joke! A type-off is a serious thing! Just get back to your desk and get to work and I won't take this infraction any further!"));
+                LevelOneConfig.Add(new TypingLine(entity: "Player", textToType: "No"));
                 LevelOneConfig.Add(new TypingLine(entity: "Enemy", textToType: "Ok then, a type-off it is. You're a terrible employee! I've kept you trapped in your position for years, hoping that one day you’d quit on your own so that I didn’t have to deal with you anymore! " +
-                    "Now you've gone ahead and saved me the trouble of having to fire you by challenging me to this type-off. And once this challenge is over, you’re going to be banished to the typing dimension! Fool!", timeLimitSeconds: -1));
-                LevelOneConfig.Add(new TypingLine(entity: "Player", textToType: "Your words are too simple to type. What is this, a third-grade reading level?", timeLimitSeconds: -1));
+                    "Now you've gone ahead and saved me the trouble of having to fire you by challenging me to this type-off. And once this challenge is over, you’re going to be banished to the typing dimension! Fool!"));
+                LevelOneConfig.Add(new TypingLine(entity: "Player", textToType: "Your words are too simple to type. What is this, a third-grade reading level?"));
                 LevelOneConfig.Add(new TypingLine(entity: "Enemy", textToType: "Guh! You can’t be serious! You defeated that entire paragraph? Those were some of my strongest words… Fine, maybe I can't defeat you." +
-                    "But this corporation isn't finished until you can take down the CEO. I'll give you the key, but know that if you go up that elevator, you'll never come back down.", timeLimitSeconds: -1));
+                    "But this corporation isn't finished until you can take down the CEO. I'll give you the key, but know that if you go up that elevator, you'll never come back down."));
                 return LevelOneConfig;
+
             case "LevelTwo":
                 List<TypingLine> LevelTwoConfig = new List<TypingLine>();
-                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "What fool dares come before the Tribunal of Executives to challenge me to a type-off?", timeLimitSeconds: -1));
-                LevelTwoConfig.Add(new TypingLine(entity: "Player", textToType: "Me!", timeLimitSeconds: -1));
-                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "Tyler Perkins? Accounting's number one imbecile? The Hominidae incapable of refilling our caffeine apparatus? Is this a practical joke?", timeLimitSeconds: -1));
-                LevelTwoConfig.Add(new TypingLine(entity: "Player", textToType: "Me!", timeLimitSeconds: -1));
-                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "You're serious... Well then, just try to out-type this!", timeLimitSeconds: -1));
+                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "What fool dares come before the Tribunal of Executives to challenge me to a type-off?", timeAllowed: 2.0));
+                LevelTwoConfig.Add(new TypingLine(entity: "Player", textToType: "Me!"));
+                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "Tyler Perkins? Accounting's number one imbecile? The Hominidae incapable of refilling our caffeine apparatus? Is this a practical joke?", timeAllowed: 2.0));
+                LevelTwoConfig.Add(new TypingLine(entity: "Player", textToType: "Me!"));
+                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "You're serious... Well then, just try to out-type this!", timeAllowed: 2.0));
                 LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "The Word Corporation is displeased to hear of your recent dissatisfaction within our company. " +
-                    "Word Corporation is not liable for any mental, physical, or psychic Infractions against your person during your employment, you have agreed to sign away these rights in your onboarding contract when joining our company.", timeLimitSeconds: -1));
+                    "Word Corporation is not liable for any mental, physical, or psychic Infractions against your person during your employment, you have agreed to sign away these rights in your onboarding contract when joining our company.", timeAllowed: 2.0));
+                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "Should our employees find any pressing issues after the signing of this contract, then they may be entitled to trial by typing in order to have their grievances properly represented. However, those who fail said trial forfeit their right to type for all " +
+                    "of eternity.", timeAllowed: 2.0));
+                LevelTwoConfig.Add(new TypingLine(entity: "Player", textToType: "Uhm... I agree to these terms and conditions?"));
+                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "You can outype a full block, but so what? Lets see how you do against my barrage attack!", timeAllowed: 2.0));
+                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "You're:", timeAllowed: 30.0, textGameTag: "PopoutWordPlaceholder1"));
+                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "cretinous", timeAllowed: 30.0, textGameTag: "PopoutWordPlaceholder2"));
+                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "lackluster", timeAllowed: 30.0, textGameTag: "PopoutWordPlaceholder3"));
+                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "malodorous", timeAllowed: 30.0, textGameTag: "PopoutWordPlaceholder4"));
+                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "elephantine", timeAllowed: 30.0, textGameTag: "PopoutWordPlaceholder2"));
+                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "reprehensible", timeAllowed: 30.0, textGameTag: "PopoutWordPlaceholder1"));
+                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "...", timeAllowed: 30.0, textGameTag: "PopoutWordPlaceholder4"));
+                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "A lil' bitch", timeAllowed: 30.0, textGameTag: "PopoutWordPlaceholder3"));
+                LevelTwoConfig.Add(new TypingLine(entity: "Enemy", textToType: "Damn, Management warned me you were good. But you actually survived my legalese AND my barrage? Fine, but I still have one final trick up my sleeve...", timeAllowed: 30.0));
                 return LevelTwoConfig;
             case "LevelThree":
                 List<TypingLine> LevelThreeConfig = new List<TypingLine>();
-                LevelThreeConfig.Add(new TypingLine(entity: "Enemy", textToType: "You are coming in late again? This is the fifth time this week. We could have you fired for this.", timeLimitSeconds: -1));
+                LevelThreeConfig.Add(new TypingLine(entity: "Enemy", textToType: "You are coming in late again? This is the fifth time this week. We could have you fired for this."));
                 return LevelThreeConfig;
             default:
                 return new List<TypingLine>();
--- Assets/TypingInput.cs
+++ Assets/TypingInput.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text.RegularExpressions;
 using TMPro;
@@ -8,55 +9,132 @@ using UnityEngine;
 
 public class TypingInput : MonoBehaviour
 {
-    private TextMeshProUGUI userInputText;
-    private TextMeshProUGUI topTextElement;
-    private TextMeshProUGUI playerDialogueTextElement;
     private string rawInput;
     private string outputText;
     private bool isInitialized = false;
-    private bool hasCompletedWord = false;
-    private bool isAdvancingLine = false;
+    private HashSet<TypingLine> spawnedLines = new HashSet<TypingLine>();
+    private Dictionary<TypingLine, GameObject> spawnedTextObjects = new();
+    private Dictionary<string, TextMeshProUGUI> uiTextElements = new();
 
     public TypingScenario currentScenario;
     public static event System.Action<TypingEventPayload> OnWordCompleted;
     public static event System.Action<string> OnLevelCompleted;
-    private List<TypingLine> activeTypingLines = new();
-    private float currentTime = 0f;
+    private List<TypingLine> activePrompts = new List<TypingLine>();
+    private TextMeshProUGUI currentActiveTextToType;
+    private string[] textTags = { "UserInput", "MainDisplayText", "PlayerDialogue", "PopoutWordPlaceholder1", "PopoutWordPlaceholder2", "PopoutWordPlaceholder3", "PopoutWordPlaceholder4" };
+
+    private double timeAtWordStart = 0;
+    private double currentTime;
+    private int currentProcessedLineIndex;
+    private TypingLine currentProcessedLine;
+
     public List<TypingLine> typingConfig;
+    public List<TypingLine> enemyLines;
     public int totalWordCount;
     public int correctWords;
 
     public void Start()
     {
         InitializeTypingScenario();
-        typingConfig = TypingConfig.GetTypingConfig(currentScenario.scenarioName);
-        correctWords = 0;
-        List<TypingLine> enemyLines = typingConfig
-            .Where(item => item.entity == "enemy")
-            .ToList();
-        List<TypingLine> playerLines = typingConfig
-            .Where(item => item.entity != "enemy")
-            .ToList();
-        totalWordCount = enemyLines
-            .SelectMany(line => line.textToType.Split((char[])null, StringSplitOptions.RemoveEmptyEntries))
-            .Count();
-
-        Debug.Log("Total character count for boss dialogue to type (excluding whitespace): " + totalWordCount);
     }
 
     void Update()
     {
+        currentTime = Time.timeSinceLevelLoadAsDouble;
         if (!isInitialized)
         {
-            TryInitializeUIReferences();
+            // TODO: Break this list up in the scriptable object assigned in the level so we don't have to maintain this list for every level in code
+            if (currentScenario.scenarioName == "LevelOne")
+            {
+                TryInitializeUIReferences(new[] { "UserInput", "MainDisplayText", "PlayerDialogue" });
+            }
+            else
+            {
+                TryInitializeUIReferences(new[] { "UserInput", "MainDisplayText", "PlayerDialogue", "PopoutWordPlaceholder1", "PopoutWordPlaceholder2", "PopoutWordPlaceholder3", "PopoutWordPlaceholder4" });
+            }
             return;
         }
 
         if (typingConfig == null || typingConfig.Count == 0) return;
 
+        if (currentScenario.scenarioName == "LevelTwo") {
+           ProcessPromptExpirations();
+        }
         ProcessUserInput();
         UpdateColoredText();
         CheckForWordCompletion();
+    }
+
+    private void ProcessPromptExpirations()
+    {
+        if (!uiTextElements.TryGetValue("UserInput", out var userInputText)) return;
+        
+        // Gone over the word time limit
+        if (currentTime > timeAtWordStart + currentProcessedLine.timeAllowed)
+        {
+            userInputText.text = "";
+            ClearTextGameObjects();
+            // Send event to remove healthbar\
+            
+            currentProcessedLineIndex += 1;
+            while (currentProcessedLineIndex < typingConfig.Count && typingConfig[currentProcessedLineIndex].entity != "Enemy")
+            {
+                currentProcessedLineIndex += 1;
+            } 
+            currentProcessedLine = typingConfig[currentProcessedLineIndex];
+            currentActiveTextToType = GameObject.FindGameObjectWithTag(currentProcessedLine.textGameTag)?.GetComponent<TextMeshProUGUI>();
+            currentActiveTextToType.text = currentProcessedLine.textToType;
+            timeAtWordStart = currentTime;
+        }
+    }
+
+    void ClearTextGameObjects()
+    {
+        uiTextElements.TryGetValue("UserInput", out var UserInputLineTextVar);
+        uiTextElements.TryGetValue("MainDisplayText", out var MainDisplayLineTextVar);
+        uiTextElements.TryGetValue("PlayerDialogue", out var playerDialogueTextVar);
+        UserInputLineTextVar.text = "";
+        MainDisplayLineTextVar.text = "";
+        playerDialogueTextVar.text = "";
+        if (currentScenario.scenarioName == "LevelTwo" || currentScenario.scenarioName == "LevelThree")
+        {
+            uiTextElements.TryGetValue("PopoutWordPlaceholder1", out var PopoutWordPlaceholder1TextVar);
+            uiTextElements.TryGetValue("PopoutWordPlaceholder2", out var PopoutWordPlaceholder2TextVar);
+            uiTextElements.TryGetValue("PopoutWordPlaceholder3", out var PopoutWordPlaceholder3TextVar);
+            uiTextElements.TryGetValue("PopoutWordPlaceholder4", out var PopoutWordPlaceholder4TextVar);
+            PopoutWordPlaceholder1TextVar.text = "";
+            PopoutWordPlaceholder2TextVar.text = "";
+            PopoutWordPlaceholder3TextVar.text = "";
+            PopoutWordPlaceholder4TextVar.text = "";
+        }
+    }
+
+    void SpawnText(TypingLine typingLine, int id)
+    {
+        if (uiTextElements.TryGetValue(typingLine.textGameTag, out var currentLineTextVar))
+        {
+            currentLineTextVar.text = typingLine.textToType;
+            currentActiveTextToType = currentLineTextVar;
+        }
+
+        // Option - Dynamically creating text in random areas for popout words - kinda janky and imo not worth it since we don't plan on having too many words at once
+        /*
+        GameObject textGO = new GameObject("TimedText");
+        GameObject canvasGameObject = GameObject.FindGameObjectWithTag("InnerCanvas").gameObject;
+
+        textGO.transform.SetParent(canvasGameObject.transform, false);
+        textGO.layer = canvasGameObject.layer;
+
+        TextMeshProUGUI tmp = textGO.AddComponent<TextMeshProUGUI>();
+        tmp.text = typingLine.textToType;
+        tmp.fontSize = 36;
+
+        RectTransform rect = textGO.GetComponent<RectTransform>();
+        rect.localScale = Vector3.one;
+        rect.anchoredPosition = new Vector2(0, -50 * activePrompts.Count); // Stack vertically
+
+        spawnedTextObjects[typingLine] = textGO;
+        */
     }
 
     private void InitializeTypingScenario()
@@ -64,32 +142,51 @@ public class TypingInput : MonoBehaviour
         typingConfig = TypingConfig.GetTypingConfig(currentScenario.scenarioName);
         correctWords = 0;
 
-        var enemyLines = typingConfig
-            .Where(item => item.entity == "enemy")
+        enemyLines = typingConfig
+            .Where(item => item.entity == "Enemy")
             .ToList();
 
         totalWordCount = enemyLines
             .SelectMany(line => line.textToType.Split((char[])null, StringSplitOptions.RemoveEmptyEntries))
             .Count();
+
+        currentProcessedLineIndex = 0;
+        currentProcessedLine = typingConfig[0];
+        Debug.Log($"Initial line: {currentProcessedLine.textToType}");
+        currentActiveTextToType = GameObject.FindGameObjectWithTag(currentProcessedLine.textGameTag)?.GetComponent<TextMeshProUGUI>();
+        currentActiveTextToType.text = currentProcessedLine.textToType;
     }
 
-    private void TryInitializeUIReferences()
+    private void TryInitializeUIReferences(string[] tags)
     {
-        userInputText = GameObject.FindGameObjectWithTag("UserInput")?.GetComponent<TextMeshProUGUI>();
-        topTextElement = GameObject.FindGameObjectWithTag("TopText")?.GetComponent<TextMeshProUGUI>();
-        playerDialogueTextElement = GameObject.FindGameObjectWithTag("PlayerDialogue")?.GetComponent<TextMeshProUGUI>();
-
-        if (userInputText != null && topTextElement != null && playerDialogueTextElement != null)
+        foreach (string tag in tags)
         {
-            isInitialized = true;
-            userInputText.text = "";
-            playerDialogueTextElement.text = "";
+            GameObject go = GameObject.FindGameObjectWithTag(tag);
+            if (go != null)
+            {
+                TextMeshProUGUI tmp = go.GetComponent<TextMeshProUGUI>();
+                if (tmp != null)
+                {
+                    uiTextElements[tag] = tmp;
+                    if (currentScenario.scenarioName == "LevelTwo" || currentScenario.scenarioName == "LevelThree") tmp.text = "";
+                }
+                else
+                {
+                    Debug.LogWarning($"No TextMeshProUGUI on GameObject with tag '{tag}'");
+                }
+            }
+            else
+            {
+                return;
+            }
         }
+        isInitialized = true;
     }
 
     private void ProcessUserInput()
     {
-        rawInput = StripColorTags(userInputText.text);
+        uiTextElements.TryGetValue("UserInput", out var currentLineTextVar);
+        rawInput = StripColorTags(currentLineTextVar?.text ?? "");
 
         foreach (char c in Input.inputString)
         {
@@ -106,7 +203,10 @@ public class TypingInput : MonoBehaviour
 
     private void UpdateColoredText()
     {
-        string targetText = topTextElement.text ?? "";
+        if (!uiTextElements.TryGetValue("MainDisplayText", out var mainDisplayText)) return;
+        if (!uiTextElements.TryGetValue("UserInput", out var userInputText)) return;
+
+        string targetText = currentActiveTextToType.text;
         int divergenceIndex = GetDivergenceIndex(rawInput, targetText);
 
         if (divergenceIndex == 0)
@@ -129,18 +229,16 @@ public class TypingInput : MonoBehaviour
 
     private void CheckForWordCompletion()
     {
-        string targetText = topTextElement.text ?? "";
-
+        if (!uiTextElements.TryGetValue("UserInput", out var userInputText)) return;
+        string targetText = currentActiveTextToType.text;
         if (rawInput == targetText)
         {
             correctWords += rawInput.Split((char[])null, StringSplitOptions.RemoveEmptyEntries).Length;
             OnWordCompleted?.Invoke(new TypingEventPayload(rawInput, totalWordCount, correctWords));
-            typingConfig.RemoveAt(0);
-            
+
             rawInput = "";
             outputText = "";
             userInputText.text = "";
-
             AdvanceToNextTypingLine();
         }
     }
@@ -148,19 +246,25 @@ public class TypingInput : MonoBehaviour
     //Could change this to a coroutine to introduce a delay before going to the next line - used for an animation
     private void AdvanceToNextTypingLine()
     {
-        while (typingConfig.Count > 0 && typingConfig[0].entity == "Player")
-        {
-            playerDialogueTextElement.text = typingConfig[0].textToType;
-            typingConfig.RemoveAt(0);
-        }
+        if (!uiTextElements.TryGetValue("PlayerDialogue", out var playerDialogueTextVar)) return;
+        if (!uiTextElements.TryGetValue("MainDisplayText", out var mainDisplayTextVar)) return;
 
-        if (typingConfig.Count > 0)
+        ClearTextGameObjects();
+        Debug.Log($"Advancing to next line at line: {currentProcessedLine.textToType}");
+        currentProcessedLineIndex += 1;
+        while (currentProcessedLineIndex < typingConfig.Count && typingConfig[currentProcessedLineIndex].entity != "Enemy")
         {
-            topTextElement.text = typingConfig[0].textToType;
+            playerDialogueTextVar.text = typingConfig[currentProcessedLineIndex].textToType;
+            currentProcessedLineIndex += 1;
         }
-        else
+        currentProcessedLine = typingConfig[currentProcessedLineIndex];
+        currentActiveTextToType = GameObject.FindGameObjectWithTag(currentProcessedLine.textGameTag)?.GetComponent<TextMeshProUGUI>();
+        currentActiveTextToType.text = currentProcessedLine.textToType;
+        timeAtWordStart = currentTime;
+
+        if( currentProcessedLineIndex >= typingConfig.Count )
         {
-            topTextElement.text = "";
+            mainDisplayTextVar.text = "";
             OnLevelCompleted?.Invoke(currentScenario.scenarioName);
         }
     }
@@ -206,4 +310,11 @@ public class ActiveTypingLine
     public TypingLine line;
     public float expirationTime;
     public GameObject textObject; // To display it on screen
+}
+
+// Used for levels 2 and 3
+public class TimedText
+{
+    public TextMeshProUGUI textObject;
+    public float expirationTime;
 }
