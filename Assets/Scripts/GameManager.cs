dc5bbd9eb793afe1844f49481b5ad45f37c7e1a1
rohanbhuyan <bhuyan.rohan@outlook.com>
2025-07-10 00:15:09 -0400

    init animations

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public int currentLevel = 1;
+
+    public Sprite bossIdle;
+    public Sprite bossAtk;
+    public Sprite bossHurt;
+    public Sprite bossHurt2;
+    public Sprite playerIdle;
+    public Sprite playerAtk;
+    public Sprite playerAtk2;
+
+    public SpriteRenderer playerSprite;
+    public SpriteRenderer bossSprite;
 
     void OnEnable()
     {
@@ -23,11 +35,30 @@ public class GameManager : MonoBehaviour
     void HandleWordCompletion(TypingEventPayload completedWord)
     {
         Debug.Log("Completed word: " + completedWord.word);
+        StartCoroutine(WordCompleteAnimations());
     }
     void HandleLevelCompletion(string scenarioName)
     {
         Debug.Log("Level completed: " + scenarioName);
         currentLevel++;
         //end current level and play a cut scene or something to move to next level or end game
+    }
+
+    IEnumerator WordCompleteAnimations()
+    {
+        int randomPic = Random.Range(0, 2);
+        if (randomPic == 0)
+        {
+            playerSprite.sprite = playerAtk;
+            bossSprite.sprite = bossHurt;
+        }
+        else if (randomPic == 1)
+        {
+            playerSprite.sprite = playerAtk2;
+            bossSprite.sprite = bossHurt2;
+        }
+        yield return new WaitForSeconds(.5f);
+        playerSprite.sprite = playerIdle;
+        bossSprite.sprite = bossIdle;
     }
 }
